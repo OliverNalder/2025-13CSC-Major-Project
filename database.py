ac48a34a03e21465f4d7df0fe705851b5d465c47
OliverNalder <on19053@students.sacredheart.school.nz>
2025-05-27 14:24:48 +1200

    Started working on databases

---
+++ database.py
@@ -0,0 +1,9 @@
+import sqlite3
+conn = sqlite3.connect('csv.db')
+conn.execute("CREATE TABLE acc_info (acc_id INTEGER PRIMARY KEY, username char(100) NOT NULL, password char(100) NOT NULL, manager INTEGER)")
+conn.commit
+
+conn = sqlite3.connect('csv.db')
+conn.execute("CREATE TABLE teams (team_id INTEGER PRIMARY KEY, team_name char(100) NOT NULL)")
+conn.commit
+
