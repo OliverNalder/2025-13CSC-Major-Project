7d72ef0bc1922654a8197f37b9bb6d3a73e0c8f0
OliverNalder <on19053@students.sacredheart.school.nz>
2025-06-13 14:24:15 +1200

    Sign in page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -10,7 +11,14 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-@app.route("/signin")
+@app.route("/signin", method="GET")
 def login():
+    if request.GET.save:
+        username = request.GET.username.strip()
+
+        conn = sqlite3.connect("csv.db")
+        c = conn.cursor()
+        c.execute(f"SELECT username FROM acc_info WHERE username LIKE {username}")
+
 
     return render_template('sign_in.html')
