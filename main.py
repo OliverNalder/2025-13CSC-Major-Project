2bf6c0202c6003b2ad35d770ed201b3ea6f6c228
OliverNalder <on19053@students.sacredheart.school.nz>
2025-06-16 10:45:15 +1200

    signin + signup

--- main.py
+++ main.py
@@ -7,18 +7,46 @@ app = Flask(__name__)
 def main():
     return render_template('myReports.html')
 
+
+@app.route("/signin", methods=["GET"])
+def login():
+    error_message = ''
+    if 'save' in request.args:
+        username = request.args.get('username', '').strip()
+        try:
+            conn = sqlite3.connect("csv.db")
+            c = conn.cursor()
+            c.execute(f"SELECT username FROM acc_info WHERE username LIKE {username}")
+            checker = c.fetchall()
+            c.close()
+            print(checker)
+        except sqlite3.OperationalError:
+            error_message = 'Couldn\'t find your account, Please try again'
+        
+
+
+    return render_template('sign_in_username.html', error_message=error_message)
+
+@app.route("/signup", methods=["GET"])
+def login():
+    error_message = ''
+    if 'save' in request.args:
+        username = request.args.get('username', '').strip()
+        try:
+            conn = sqlite3.connect("csv.db")
+            c = conn.cursor()
+            c.execute(f"SELECT username FROM acc_info WHERE username LIKE {username}")
+            checker = c.fetchall()
+            c.close()
+            error_message = 'Couldn\'t find your account, Please try again'
+        except sqlite3.OperationalError:
+            print(checker)
+        
+
+
+    return render_template('sign_up_username.html', error_message=error_message)
+
 if __name__ == "__main__":
     app.run(debug=True)
 
 
-@app.route("/signin", method="GET")
-def login():
-    if request.GET.save:
-        username = request.GET.username.strip()
-
-        conn = sqlite3.connect("csv.db")
-        c = conn.cursor()
-        c.execute(f"SELECT username FROM acc_info WHERE username LIKE {username}")
-
-
-    return render_template('sign_in.html')
