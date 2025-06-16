36ca93a8942a3a7c9c0043ace1fac68f90f6f6e6
OliverNalder <on19053@students.sacredheart.school.nz>
2025-06-16 22:24:56 +1200

    Signin + Signup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -9,14 +9,14 @@ def main():
 
 
 @app.route("/signin", methods=["GET"])
-def login():
+def signin():
     error_message = ''
     if 'save' in request.args:
         username = request.args.get('username', '').strip()
         try:
             conn = sqlite3.connect("csv.db")
             c = conn.cursor()
-            c.execute(f"SELECT username FROM acc_info WHERE username LIKE {username}")
+            c.execute(f"SELECT username FROM acc_info WHERE username = {username}")
             checker = c.fetchall()
             c.close()
             print(checker)
@@ -28,23 +28,41 @@ def login():
     return render_template('sign_in_username.html', error_message=error_message)
 
 @app.route("/signup", methods=["GET"])
-def login():
+def signup():
     error_message = ''
     if 'save' in request.args:
-        username = request.args.get('username', '').strip()
-        try:
+            username = request.args.get('username', '').strip()
+        
             conn = sqlite3.connect("csv.db")
             c = conn.cursor()
-            c.execute(f"SELECT username FROM acc_info WHERE username LIKE {username}")
+            c.execute(f"SELECT acc_id FROM acc_info WHERE username LIKE {username}")
             checker = c.fetchall()
             c.close()
-            error_message = 'Couldn\'t find your account, Please try again'
-        except sqlite3.OperationalError:
             print(checker)
+            if checker:
+                error_message = 'Couldn\'t find your account, Please try again'
+            return render_template('sign_up_username.html', error_message=error_message)
         
-
-
+        
     return render_template('sign_up_username.html', error_message=error_message)
+
+
+    
+
+@app.route("/signup/username=<username>", methods=["GET"])
+def signup_2(username):
+    error_message = ''
+    if 'save' in request.args:
+        password = request.args.get('password', '').strip()
+        conn = sqlite3.connect(f'csv.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO acc_info (username, password, manager) VALUES (?, ?, ?)", (username, password, 1))
+        conn.commit()
+        c.close()
+        return redirect('/')
+    else:
+        return render_template('sign_up_password.html', error_message=error_message, username=username)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
