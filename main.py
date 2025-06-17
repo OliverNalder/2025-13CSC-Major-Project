0da1b3fa22f15249d8a44d76ee17181daf7152c4
OliverNalder <on19053@students.sacredheart.school.nz>
2025-06-17 14:25:23 +1200

    Signin and Signup page

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def signin():
         try:
             conn = sqlite3.connect("csv.db")
             c = conn.cursor()
-            c.execute(f"SELECT username FROM acc_info WHERE username = {username}")
+            c.execute(f"SELECT acc_id FROM acc_info WHERE username = '{username}'")
             checker = c.fetchall()
             c.close()
             print(checker)
@@ -32,16 +32,17 @@ def signup():
     error_message = ''
     if 'save' in request.args:
             username = request.args.get('username', '').strip()
-        
-            conn = sqlite3.connect("csv.db")
-            c = conn.cursor()
-            c.execute(f"SELECT acc_id FROM acc_info WHERE username LIKE {username}")
-            checker = c.fetchall()
-            c.close()
-            print(checker)
-            if checker:
-                error_message = 'Couldn\'t find your account, Please try again'
-            return render_template('sign_up_username.html', error_message=error_message)
+            try:
+                conn = sqlite3.connect("csv.db")
+                c = conn.cursor()
+                c.execute(f"SELECT username FROM acc_info WHERE username LIKE '{username}'")
+                checker = c.fetchall()
+                c.close()
+                if checker[0][0] == username:
+                    error_message = 'This Name is Already in Use'
+                    return render_template('sign_up_username.html', error_message=error_message)
+            except IndexError:
+                return redirect(f'/signup/username={username}')
         
         
     return render_template('sign_up_username.html', error_message=error_message)
@@ -54,12 +55,22 @@ def signup_2(username):
     error_message = ''
     if 'save' in request.args:
         password = request.args.get('password', '').strip()
-        conn = sqlite3.connect(f'csv.db')
-        c = conn.cursor()
-        c.execute("INSERT INTO acc_info (username, password, manager) VALUES (?, ?, ?)", (username, password, 1))
-        conn.commit()
-        c.close()
-        return redirect('/')
+        try:
+            conn = sqlite3.connect("csv.db")
+            c = conn.cursor()
+            c.execute(f"SELECT username FROM acc_info WHERE username LIKE '{username}'")
+            checker = c.fetchall()
+            c.close()
+            if checker[0][0] == username:
+                error_message = 'This Name is Already in Use'
+                return render_template('sign_up_username.html', error_message=error_message)
+        except IndexError:
+            conn = sqlite3.connect(f'csv.db')
+            c = conn.cursor()
+            c.execute("INSERT INTO acc_info (username, password, manager) VALUES (?, ?, ?)", (username, password, 1))
+            conn.commit()
+            c.close()
+            return redirect('/')
     else:
         return render_template('sign_up_password.html', error_message=error_message, username=username)
 
